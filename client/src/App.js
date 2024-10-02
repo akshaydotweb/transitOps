20533189eebc4e2056569ddef0f16d9d5c3e3aae
Akshay V <78274108+akshaydotweb@users.noreply.github.com>
2024-10-02 10:51:13 +0530

    created a server which connects to mysql localhost

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
-'use client';
-import { EmailPasswordAuth } from "supertokens-auth-react/recipe/emailpassword";
 
-function ClientDashboard() {
-    return (
-        <EmailPasswordAuth>
-            <div>Client Dashboard</div>
-        </EmailPasswordAuth>
-    );
-}
+// function ClientDashboard() {
+//     return (
+//         <EmailPasswordAuth>
+//             <div>Client Dashboard</div>
+//         </EmailPasswordAuth>
+//     );
+// }
 
+// export default ClientDashboard;
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import EmailPasswordAuth  from "supertokens-auth-react/recipe/emailpassword";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <p>This is client Dashboard</p>
   </React.StrictMode>
 );
 
